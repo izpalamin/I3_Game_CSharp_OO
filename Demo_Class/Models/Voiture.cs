557122732391ed59ad839dc4db26a4768924cdbf
Isaline Palamin <palaminisaline@gmail.com>
2025-06-02 14:27:23 +0200

    suite des exercices

---
+++ Demo_Class/Models/Voiture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Class.Models
+{
+    internal class Voiture
+    {
+        public string immatriculation;
+        public string couleur;
+        public int nbRoues;
+
+    }
+}
---
+++ Demo_Class/Program.cs
@@ -0,0 +1,27 @@
+﻿using Demo_Class.Models;
+
+namespace Demo_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Voiture maFerrari = new Voiture()
+            {
+                couleur = "Rouge",
+                immatriculation = "2-ABC-123",
+                nbRoues = 4
+            };
+            Voiture laFraise = maFerrari;
+            Console.WriteLine($"Voici ma voiture {maFerrari.couleur}, immatricullée {maFerrari.immatriculation}.");
+            Console.WriteLine($"Voici la voiture {laFraise.couleur} de mon fils, immatriculée {maFerrari.immatriculation}.");
+            Console.WriteLine($"Je roule à pleine vitesse avec les {maFerrari.nbRoues} roues de ma voiture!");
+            Console.WriteLine("BOOM!!!!!!!");
+
+            maFerrari.nbRoues--;
+
+            Console.WriteLine($"Ma voiture a maintenant {maFerrari.nbRoues} roues ...");
+            Console.WriteLine($"La voiture de mon fils a maintenant {laFraise.nbRoues} roues ...");
+        }
+    }
+}
