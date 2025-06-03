dbdc9d6bd7cb60a74ba60ff04c9da320ebe76c28
Isaline Palamin <palaminisaline@gmail.com>
2025-06-03 16:04:36 +0200

    Demo_Encapsulation

---
+++ Demo_Encapsulation/Models/Login.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Encapsulation.Models
+{
+    internal class Login
+    {
+        public string email;
+        private string _password;
+
+        public bool ChangePassword(string password)
+        {
+            if (password is null)
+            {
+                if (password is null)
+                    _password = password;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool CheckUser(string email, string password)
+        {
+            return this.email == email && this._password == password;
+        }
+    }
+}
---
+++ Demo_Encapsulation/Program.cs
@@ -0,0 +1,36 @@
+﻿using Demo_Encapsulation.Models;
+
+namespace Demo_Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Login monLogin = new Login();
+            monLogin.email = "palaminisaline@gmail.com";
+            monLogin.ChangePassword("blabla$\r");
+            Console.WriteLine("Veuillez introduire votre email :");
+            monLogin.email = Console.ReadLine();
+            Console.WriteLine("Veuillez introduire votre mot de passe.");
+            ConsoleKeyInfo keyInfo;
+            string password = "";
+            do
+            {
+                keyInfo = Console.ReadKey(true);
+                password += keyInfo.KeyChar;
+                Console.Write("*");
+            }
+            while (keyInfo.Key != ConsoleKey.Enter);
+            Console.WriteLine();
+            bool isConnected = monLogin.CheckUser(email,password);
+            if (isConnected)
+            {
+                Console.WriteLine("Bienvenue!");
+            }
+            else
+            {
+                Console.WriteLine("Compte bloqué!");
+            }
+        }
+    }
+}
